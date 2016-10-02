eb2494f213fa4b2455e141ac0e153924a32766bf
Leonidas Tsementzis <leotsem@gmail.com>
2016-10-02 19:53:04 +0100

    Replace `iteritems()` with `items()`

--- django_elasticsearch/utils.py
+++ django_elasticsearch/utils.py
@@ -2,7 +2,7 @@ import collections
 
 
 def nested_update(d, u):
-    for k, v in u.iteritems():
+    for k, v in u.items():
         if isinstance(v, collections.Mapping):
             r = nested_update(d.get(k, {}), v)
             d[k] = r
@@ -20,4 +20,4 @@ def dict_depth(d, depth=0):
     if not isinstance(d, dict) or not d:
         return depth
     return max(dict_depth(v, depth + 1)
-               for k, v in d.iteritems())
+               for k, v in d.items())
